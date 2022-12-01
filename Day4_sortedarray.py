cd4f6bf6dc4a2560188cf1e25edde0e9d3d23e56
Jainika123 <jainikadaga30082001@gmail.com>
2022-12-01 18:43:19 +0530

    Array_based_questions_solved_by_python

---
+++ Day4_sortedarray.py
@@ -0,0 +1,24 @@
+#Chech if an array is sorted in non decreasing order
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    def searchInSorted(self,arr, N, K):
+        #Your code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import math
+if __name__ == '__main__': 
+	t = int(input())
+	for _ in range(t):
+		NK = input().strip().split()
+		N = int(NK[0])
+		K = int(NK[1])
+		A = [ int(x) for x in input().strip().split() ]
+		ob=Solution()
+		print(ob.searchInSorted(A,N,K))
+
+# } Driver Code Ends
